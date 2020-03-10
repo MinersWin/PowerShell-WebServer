48930cd525fa39af8f2b2b7fc4cba11c523709af
MinersWin <minerswin@minerswin.de>
2020-03-10 15:48:32 +0100

    Added few Functions in GUI

--- WebServer.ps1
+++ WebServer.ps1
@@ -4,6 +4,7 @@ Add-Type -AssemblyName System.Windows.Forms
 $TextBoxPath.Text = $MyDir
 $TextBoxHTTPPort.Text = "HTTP Port eintragen: [80,8080]"
 $TextBoxHTTPSPort.Text = "HTTPS Port eintragen [443]"
+$TextBoxHosts.Text = "Hosts eintragen [localhost,127.0.0.1,192.168.0.1,10.10.10.42]"
 $ButtonStart.Add_Click{(Start-WebServer)}
 
 $Button5.Add_Click{
@@ -26,8 +27,20 @@ function Start-WebServer{
         default {($HTTPSPort)}
     }
     Write-Host "Der Webserver hört über HTTP auf folgende Ports: $($HTTPSPort)"
+    if ($CheckBoxDirectory.Checked){
+        $DirectoryCommandAddOn = "-NoFolderListing"
+        Write-Host "Zeige keine Ordner"
+    } else {
+        $DirectoryCommandAddOn = ""
+        Write-Host "Zeigne Ordner"
+    }
+    if ($TextBoxHosts.Text -eq "Hosts eintragen [localhost,127.0.0.1,192.168.0.1,10.10.10.42]"){
+        $Hosts = "*"
+    } else {
+        $Hosts = "$TextBosHosts.Text"
+    }
     $Pfad = $TextBoxPath.Text
-    Start-Process powershell ".\WebServer_Logic.ps1 -root $($Pfad) -Https $($HTTPSPort) -Http $($HTTPPort)"
+    Start-Process powershell ".\WebServer_Logic.ps1 -root $($Pfad) -Https $($HTTPSPort) -Http $($HTTPPort) $DirectoryCommandAddOn $Hosts"
 }
 
 
